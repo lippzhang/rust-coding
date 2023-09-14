f63589c48f6cecc676df2213dc2ccf8ebf8e8305
lippzhang <489435760@qq.com>
2023-09-14 16:51:44 +0800

    feat: log execrise

--- big_program/crates/cli/src/main.rs
+++ big_program/crates/cli/src/main.rs
@@ -1,12 +1,36 @@
 use my_lib::Module;
+use log::{info, error, debug};
+use env_logger;
+use structopt::{clap::AppSettings, StructOpt};
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "wasm-runner",
+    about = "Sample project from https://vino.dev/blog/node-to-rust-day-1-rustup/",
+    global_settings(&[
+      AppSettings::ColoredHelp
+    ]),
+)]
+struct CliOptions {
+     /// The WebAssembly file to load.
+     #[structopt(parse(from_os_str))]
+     pub(crate) file_path: PathBuf,
+}
 
 fn main() {
-    match Module::from_file("./module.wasm") {
+    env_logger::init();
+    debug!("Initialized logger");
+
+    let options = CliOptions::from_args();
+
+    match Module::from_file(&options.file_path) {
         Ok(_) => {
-          println!("Module loaded");
+            info!("Module loaded");
         }
         Err(e) => {
-          println!("Module failed to load: {}", e);
+            error!("Module failed to load: {}", e);
         }
       }
 }
+
--- big_program/crates/my-lib/src/lib.rs
+++ big_program/crates/my-lib/src/lib.rs
@@ -1,9 +1,11 @@
 use std::path::Path;
+use log::debug;
 
 pub struct Module {}
 
 impl Module {
     pub fn from_file<T: AsRef<Path>>(path: T) -> Result<Self, std::io::Error> {
+        debug!("Loading wasm file from {:?}", path.as_ref());
         Ok(Self{})
     }
 }
